3b35bc7211f514e9ed80c07937dc21e4eac214ce
Anurag Tiwari <tiwari136@gmail.com>
2020-11-24 01:08:07 +0530

    Fetching Products From the backend

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,9 @@ app.get('/api/products', (req,res)=>{
     res.json(products)
 } )
 
-app.get('/api/products/:id', (req,res)=>{
-    const product = products.find(p=>p._id = req.params.id);
+app.get('/api/products/:id', async (req,res)=>{
+    const product = await products.find(p=>p._id == req.params.id);
+    
     res.json(product)
 } )
 
