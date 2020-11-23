d0f781cb41b5893b024bd96a31a8fa3ae0da50b8
Anurag Tiwari <tiwari136@gmail.com>
2020-11-24 00:15:59 +0530

    Setting paths in backend for fetching products

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const products = require('./data/products')
+
+
+app.get('/', (req,res)=>{
+    res.send('Api is Running....')
+} )
+
+
+app.get('/api/products', (req,res)=>{
+    res.json(products)
+} )
+
+app.get('/api/products/:id', (req,res)=>{
+    const product = products.find(p=>p._id = req.params.id);
+    res.json(product)
+} )
+
+
+
+app.listen(5000, ()=>{
+    console.log("Listeining on port 5000")
+})
