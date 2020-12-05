8591612a8e9bfbf89a257da89af4234dfcf3ccdd
Anurag Tiwari <tiwari136@gmail.com>
2020-12-06 00:35:23 +0530

    cart funtionality : additems,removeitems,totalcost

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,11 +10,11 @@ const reducer = combineReducers({
     productDetails:productDetailsReducer,
     cart:cartReducer,
 })
-
+console.log(localStorage.getItem('cartItems'))
 const cartItemsFromStorage = localStorage.getItem('cartItems')?JSON.parse(localStorage.getItem('cartItems')):[]
 
 const initialState = {
-    cart:{cartItems:cartItemsFromStorage}
+cart:{cartItems:cartItemsFromStorage}
 }
 
 const middleware = [thunk]
