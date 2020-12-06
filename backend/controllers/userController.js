2939353606375d19697374128f750fdf63c958ee
Anurag Tiwari <tiwari136@gmail.com>
2020-12-07 00:41:25 +0530

    User Registration & password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,6 +27,50 @@ const  {email,password} = req.body
 })
 
 
+
+
+// @desc  Register a new user
+// @route  POST /api/users
+// @access public
+const registerUser = asyncHandler(async(req,res) =>{
+    const  {name,email,password} = req.body
+    
+     const userExists = await User.findOne({email:email})
+     
+     if(userExists){
+         res.status(400)
+         throw new Error('User already Exists')
+     }
+
+     const user =await User.create(
+         {name,
+         email,
+         password
+        }
+     )
+
+        if(user){
+            res.status(201).json(
+                {
+                    _id:user._id,
+                    name:user.name,
+                    email:user.email,
+                    isAdmin:user.isAdmin,
+                    token:generateToken(user._id)
+                }
+            )
+        }else{
+            res.status(400)
+            throw new Error('Invalid User Data')
+        }
+
+    
+    }
+    )
+
+
+
+
 // @desc  Get user profile
 // @route  GET /api/users/profile
 // @access private
@@ -48,4 +92,4 @@ const getUserProfile = asyncHandler(async(req,res) =>{
 
 })
 
-export {authUser,getUserProfile}
+export {authUser,getUserProfile,registerUser}
