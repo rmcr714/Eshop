d7026336f5aa63d1b5cfbcddb3c82f2adcab0203
Anurag Tiwari <tiwari136@gmail.com>
2020-12-09 01:23:06 +0530

    User login reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,23 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import {productListReducer,productDetailsReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
-
+import {userLoginReducer} from './reducers/userReducers.js'
 
 const reducer = combineReducers({
     productList:productListReducer,
     productDetails:productDetailsReducer,
     cart:cartReducer,
+    userLogin:userLoginReducer
+
 })
-console.log(localStorage.getItem('cartItems'))
+
+const userInfoFromStorage = localStorage.getItem('userInfo')?JSON.parse(localStorage.getItem('userInfo')):null
+
 const cartItemsFromStorage = localStorage.getItem('cartItems')?JSON.parse(localStorage.getItem('cartItems')):[]
 
 const initialState = {
-cart:{cartItems:cartItemsFromStorage}
+cart:{cartItems:cartItemsFromStorage},
+userLogin:{userInfo:userInfoFromStorage}
 }
 
 const middleware = [thunk]
