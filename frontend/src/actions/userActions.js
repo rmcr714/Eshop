e1b21c4559646996075d5fdefd9c4a8d7105d0d6
Anurag Tiwari <tiwari136@gmail.com>
2020-12-12 16:47:13 +0530

    User Register,action,screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT , USER_REGISTER_REQUEST} from "../constants/userConstants"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT ,USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,USER_REGISTER_FAIL} from "../constants/userConstants"
 
 export const login = (email,password) => async(dispatch)=>{
     try{
@@ -50,7 +51,12 @@ export const register = (name,email,password) => async(dispatch)=>{
             }
         }
 
-        const {data} = await axios.post('api/users/login', {email,password},config)
+        const {data} = await axios.post('api/users', {name,email,password},config)
+
+        dispatch({
+            type:USER_REGISTER_SUCCESS,
+            payload :data
+        })
 
         dispatch({
             type:USER_LOGIN_SUCCESS,
@@ -60,7 +66,7 @@ export const register = (name,email,password) => async(dispatch)=>{
         localStorage.setItem('userInfo',JSON.stringify(data))
 
     }catch(error){
-        dispatch({type:USER_LOGIN_FAIL,
+        dispatch({type:USER_REGISTER_FAIL,
             payload:error.response && error.response.data.message ? error.response.data.message:error.message})
 
     }
--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -4,13 +4,17 @@ import {Form,Col,Row,Button} from 'react-bootstrap'
 import {useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { login } from '../actions/userActions'
+import { register } from '../actions/userActions'
 import FormContainer from '../components/FormContainer'
 
-const LoginScreen = ({location,history}) => {
+const RegisterScreen = ({location,history}) => {
 
+    const [name,setName] = useState('')
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
+    const [confirmPassword,setConfirmPassword] = useState('')
+    const [message,setMessage] = useState(null)
+
     console.log(location.search)
     const redirect = location.search ? location.search.split('=')[1]:'/'
     console.log(redirect)
@@ -18,9 +22,9 @@ const LoginScreen = ({location,history}) => {
     
     const dispatch = useDispatch()
 
-    const userLogin = useSelector(state => state.userLogin)
+    const userRegister = useSelector(state => state.userRegister)
 
-    const {loading,userInfo,error} = userLogin
+    const {loading,userInfo,error} = userRegister
 
     useEffect(()=>{
         if(userInfo){
@@ -30,16 +34,27 @@ const LoginScreen = ({location,history}) => {
 
     const submitHandler = (e)=>{
         e.preventDefault()
-        dispatch(login(email,password))
-        //Dispatch Login
+
+        if(password!==confirmPassword){
+            setMessage('Passwords do not match')
+        }else{
+        dispatch(register(name,email,password))
+        }
+        
     }
 
     return (
         <FormContainer>
-            <h1>Sign In</h1>
+            <h1>Sign Up</h1>
+            {message && <Message variant = 'danger'>{message}</Message>}
             {error && <Message variant = 'danger'>{error}</Message>}
             {loading && <Loader></Loader>}
             <Form onSubmit = {submitHandler}>
+                <Form.Group controlId = 'name'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type = 'name' placeholder = 'Enter name' value = {name}
+                    onChange = {(e)=>setName(e.target.value)}></Form.Control>
+                </Form.Group>
                 <Form.Group controlId = 'email'>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control type = 'email' placeholder = 'Enter email' value = {email}
@@ -50,15 +65,23 @@ const LoginScreen = ({location,history}) => {
                     <Form.Control type = 'password' placeholder = 'Enter password' value = {password}
                     onChange = {(e)=>setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
-                <Button  type = 'submit' variant = 'primary'>Sign in</Button>
+
+                <Form.Group controlId = 'confirmPassword'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type = 'password' placeholder = 'Confirm Password' value = {confirmPassword}
+                    onChange = {(e)=>setConfirmPassword(e.target.value)}></Form.Control>
+                </Form.Group>
+                <Button  type = 'submit' variant = 'primary'>Register</Button>
             </Form>
             <Row className = 'py-3'>
                 <Col>
-    New Customer?<Link to = {redirect?`/register?redirect = ${redirect}`:`/register`}>{' '}Register</Link>
+                Haven an Account{' '}
+                <Link to = {redirect?`/login?redirect = ${redirect}`:`/login`}>
+                    Login</Link>
                 </Col>
             </Row>
         </FormContainer>
     )
 }
 
-export default LoginScreen
+export default RegisterScreen
