cb18c5964ff68255dde7ae52e5a23c4f7caff868
Anurag Tiwari <tiwari136@gmail.com>
2020-12-19 22:20:55 +0530

    Order controller and route

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const app = express()
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 connectDB()         //Connecting to Db 
 
@@ -19,6 +20,7 @@ app.get('/', (req,res)=>{
 
 app.use('/api/products' , productRoutes)
 app.use('/api/users' , userRoutes)
+app.use('/api/orders' , orderRoutes)
 
 
 app.use(notFound)
