edae68bf04ba03abfe212d9216e3f6d5bf793a4a
Anurag Tiwari <tiwari136@gmail.com>
2020-12-21 01:02:12 +0530

    Order pay reducer and action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -3,7 +3,11 @@ import {ORDER_CREATE_REQUEST,
     ORDER_CREATE_FAIL,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
-    ORDER_DETAILS_FAIL
+    ORDER_DETAILS_FAIL,
+    ORDER_PAY_RESET,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_SUCCESS,
+    ORDER_PAY_REQUEST
 
 } from '../constants/orderConstants'
 
@@ -55,3 +59,35 @@ export const orderDetailsReducer = (state = {loading:true,orderItems:[],shippigA
     }
 
 }
+
+
+
+
+export const orderPayReducer = (state = {},action) => {
+    switch(action.type){
+        case ORDER_PAY_REQUEST:
+            return {
+                loading:true
+            }
+        case ORDER_PAY_SUCCESS:
+            return{
+                loading:false,
+                success:true
+            }
+        case ORDER_PAY_FAIL:
+            return{
+                loading:false,
+                error:action.payload
+            }
+        case ORDER_PAY_RESET:
+            return  {} 
+        default:
+            return state
+    }
+
+}
+
+
+
+
+
