51a9ebdc78a2f7a6d5b10c73efad23881731f563
Anurag Tiwari <tiwari136@gmail.com>
2020-12-24 01:29:31 +0530

    User Details and Order Reset on logout

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -10,7 +10,8 @@ import {ORDER_CREATE_REQUEST,
     ORDER_PAY_REQUEST,
     ORDER_LIST_MY_REQUEST,
     ORDER_LIST_MY_SUCCESS,
-    ORDER_LIST_MY_FAIL
+    ORDER_LIST_MY_FAIL,
+    ORDER_LIST_MY_RESET
 
 } from '../constants/orderConstants'
 
@@ -108,6 +109,8 @@ export const orderListMyReducer = (state = {orders:[]},action) => {
                 loading:false,
                 error:action.payload
             }
+        case ORDER_LIST_MY_RESET:
+            return {orders:[]}
         default:
             return state
     }
