98061aaf662ec7d8ccafa0d153f32ebb17ab09f3
Anurag Tiwari <tiwari136@gmail.com>
2020-12-24 22:43:11 +0530

    admin middleware and getting all users profile data

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
-import { authUser,getUserProfile,registerUser ,updateUserProfile} from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser,getUserProfile,registerUser ,updateUserProfile,getUsers} from '../controllers/userController.js'
+import { protect,admin } from '../middleware/authMiddleware.js'
 
 
 router.post('/login',authUser)
@@ -9,6 +9,7 @@ router.post('/login',authUser)
 router.route('/profile').get(protect,getUserProfile) // or router.get('/profile',protect,getUserProfile)
 
 router.route('/').post(registerUser)
+router.route('/').get(protect,admin,getUsers)
 
 router.route('/profile').put(protect,updateUserProfile)
 
