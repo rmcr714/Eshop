610fb7ebaa5524a1290d5a2ba525e1b3f3ecb5bd
Anurag Tiwari <tiwari136@gmail.com>
2020-12-28 22:11:55 +0530

    Update product action and reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore,combineReducers , applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import {productListReducer,productDetailsReducer,productDeleteReducer,productCreateReducer} from './reducers/productReducers'
+import {productListReducer,productDetailsReducer,productDeleteReducer,productCreateReducer,productUpdateReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
 import {userLoginReducer,userRegisterReducer,userDetailsReducer,
     userUpdateProfileReducer,userListReducer,userDeleteReducer,userUpdateReducer} from './reducers/userReducers.js'
@@ -13,6 +13,7 @@ const reducer = combineReducers({
     productDetails:productDetailsReducer,
     productDelete:productDeleteReducer,
     productCreate:productCreateReducer,
+    productUpdate:productUpdateReducer,
     cart:cartReducer,
     userLogin:userLoginReducer,
     userRegister:userRegisterReducer,
@@ -25,6 +26,7 @@ const reducer = combineReducers({
     orderDetails:orderDetailsReducer,
     orderPay:orderPayReducer,
     orderListMy:orderListMyReducer,
+    
     
 
 })
